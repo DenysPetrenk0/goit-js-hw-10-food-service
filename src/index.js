907025cea6ba0093cfcbe8f76df2f36852f4f15e
DenysPetrenk0 <p.gesser2015@gmail.com>
2021-06-24 22:38:46 +0300

    homework10 done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-import './sass/main.scss';
+import menuData from './menu';
+import createMarkup from './templates/markup.hbs';
+
+const ulMenu = document.querySelector('.menu');
+const btnThem = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+
+  chahgeTheme() {
+    body.classList.toggle(this.LIGHT);
+    body.classList.toggle(this.DARK);
+  },
+};
+
+function getBodyTheme() {
+  const themeValue = localStorage.getItem('bodyTheme');
+  if (JSON.parse(themeValue) === Theme.DARK) {
+    defaultBodyTheme(Theme.DARK);
+    btnThem.checked = true;
+  }
+}
+getBodyTheme();
+
+function defaultBodyTheme(theme) {
+  body.classList.add(theme);
+}
+
+defaultBodyTheme(Theme.LIGHT);
+
+ulMenu.innerHTML = createMarkup(menuData);
+
+btnThem.addEventListener('change', event => {
+  Theme.chahgeTheme();
+  const classBody = body.getAttribute('class');
+  localStorage.setItem('bodyTheme', JSON.stringify(classBody));
+});
